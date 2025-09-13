059f1ffb512143d01e6ef448ddd21d9fe5a7bfe2
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-13 07:56:30 -0700

    Updated MongoDB connection options to include useCreateIndex

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
