e4b4825529b513437c564a1cffcff9e60d6cc7bb
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-13 07:57:46 -0700

    Add event emission for product creation and purchase in Transaction contract

--- contract.sol
+++ contract.sol
@@ -11,10 +11,14 @@ contract Transaction {
     mapping(uint => Product) public products;
     uint public productCount;
 
+    event ProductCreated(uint productId, string name, uint price, address owner);
+    event ProductPurchased(uint productId, address buyer);
+
     function createProduct(string memory _name, uint _price) public {
         require(_price > 0, "Product price must be greater than zero");
         productCount++;
         products[productCount] = Product(_name, _price, msg.sender, false);
+        emit ProductCreated(productCount, _name, _price, msg.sender);
     }
 
     function purchaseProduct(uint _productId) public payable {
@@ -24,5 +28,6 @@ contract Transaction {
 
         _product.owner = msg.sender;
         _product.isSold = true;
+        emit ProductPurchased(_productId, msg.sender);
     }
 }
